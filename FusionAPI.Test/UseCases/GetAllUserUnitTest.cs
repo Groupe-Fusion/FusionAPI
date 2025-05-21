cb5b7309f268805ad572ffe47cc44d00d6dfd8ad
Niamreg76 <84070314+Niamreg76@users.noreply.github.com>
2025-05-21 09:52:27 +0200

    add unit  tests

---
+++ FusionAPI.Test/UseCases/GetAllUserUnitTest.cs
@@ -0,0 +1,33 @@
+﻿using FusionAPI.Applicatif.UseCases;
+using FusionAPI.Domain.Models;
+using FusionAPI.Domain.Repositories.Core;
+using Moq;
+
+namespace FusionAPI.Test.UseCases
+{
+    public class GetAllUserUnitTest
+    {
+        [Fact]
+        public async void Success()
+        {
+            var userRepositoryMoq = new Mock<IUserRepository>();
+            userRepositoryMoq.Setup(x => x.GetAllUsersAsync(It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new List<User>());
+            var getAllUserUseCase = new GetAllUsersUseCase(userRepositoryMoq.Object);
+            var users = await getAllUserUseCase.ExecuteAsync(CancellationToken.None);
+            // Assert
+            Assert.NotNull(users);
+        }
+
+        [Fact]
+        public async void Failure()
+        {
+            var userRepositoryMoq = new Mock<IUserRepository>();
+            userRepositoryMoq.Setup(x => x.GetAllUsersAsync(It.IsAny<CancellationToken>()))
+                .ThrowsAsync(new Exception("Email is required."));
+            var getAllUserUseCase = new GetAllUsersUseCase(userRepositoryMoq.Object);
+            // Assert
+            await Assert.ThrowsAsync<Exception>(() => getAllUserUseCase.ExecuteAsync(CancellationToken.None));
+        }
+    }
+}
